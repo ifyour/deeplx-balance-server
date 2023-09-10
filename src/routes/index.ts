e56ffaa3e8d13678f9ac0db33e03c52c993514c6
ifyour <ifyour@outlook.com>
2023-09-10 19:06:29 +0800

    fix: readme

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,3 @@
-export default eventHandler((event) => {
-  event.node.res.writeHead(404, { 'Content-Type': 'application/json' })
-  event.node.res.end(
-    JSON.stringify({ success: false, message: "page not found" }),
-  )
+export default defineEventHandler(() => {
+  return '<h1>Nitro is Awesome!</h1>'
 })
